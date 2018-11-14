98b32ba921afcd89060c7e8774e3af66db9b7d45
Tom PoLáKoSz <polakosz@freemail.hu>
2018-11-14 22:37:02 +0100

    Adds two UnitTests to the Album endpoint

--- tests/Integration/EndPoints/AlbumEndPointTests.cs
+++ tests/Integration/EndPoints/AlbumEndPointTests.cs
@@ -1,13 +1,43 @@
 ﻿using NUnit.Framework;
+using PoLaKoSz.Deezer.EndPoints;
+using System.Threading.Tasks;
 
 namespace PoLaKoSz.Deezer.Tests.Integration.EndPoints
 {
     class AlbumEndPointTests : BaseEndPoint
     {
-        [Test]
-        public void Asdasd()
-        {
+        private AlbumEndPoint _endPoint;
+        private HttpClientMock _clientMock;
 
+
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _clientMock = new HttpClientMock();
+
+            _endPoint = new AlbumEndPoint(32, _clientMock);
+        }
+
+
+
+        [Test]
+        public async Task Check_AlbumGetter_EndPoint_Url()
+        {
+            await _endPoint.Get.Info();
+
+            Assert.That(_clientMock.LastCalledURL, Is.Not.Null);
+            Assert.That(_clientMock.LastCalledURL.AbsolutePath.StartsWith("/album/32/"));
+        }
+
+        [Test]
+        public async Task Check_AlbumSetter_EndPoint_Url()
+        {
+            _clientMock.ResponseFromServer = "false";
+            await _endPoint.Set.Rate(0);
+
+            Assert.That(_clientMock.LastCalledURL, Is.Not.Null);
+            Assert.That(_clientMock.LastCalledURL.AbsolutePath.StartsWith("/album/32/"));
         }
     }
 }
